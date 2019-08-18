f31055f2763997256e749fed1955f61455bb9b6c
Joseph Thibeault <joseph.thibeault@gmail.com>
2019-08-17 22:57:16 -0700

    edit service 2 (#4)

--- .circleci/test-split.sh
+++ .circleci/test-split.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
 target_branch='master'
-CIRCLE_BRANCH='edit-service-1'
+CIRCLE_BRANCH='edit-service-2-2'
 
 changed_files=$(git diff --name-only $target_branch..$CIRCLE_BRANCH)
-echo $changed_files
 
 # If common, models, or other files are affected, run full tests
 common_models_files=$(echo $changed_files | tr ' ' '\n' | grep -oE "^src\/services\/(common|models)/" || true)
 common_models_affected=$(echo $common_models_files | awk "{print length}")
 others_files=$(echo $changed_files | tr ' ' '\n' | grep -v "^src\/services\/" || true)
 others_affected=$(echo $others_files | awk "{print length}")
-services=$(echo $changed_files | tr ' ' '\n' | grep -oE "^src\/services\/(.*)/" || true)
-services_affected=$(echo $services | awk "{print length}")
+services_files=$(echo $changed_files | tr ' ' '\n' | grep -oE "^src\/services\/(.*)/" || true)
+services_affected=$(echo $services_files | awk "{print length}")
 
-if [ ${#services_affected} -gt 0 ] && [ ${#common_models_affected} -eq 0 ] && [ ${#others_affected} -eq 0 ]; then
+if [ ${#services_affected} -gt 1 ] && [ ${#common_models_affected} -eq 1 ] && [ ${#others_affected} -eq 1 ]; then
     npm test -- $services
 else
     npm test
