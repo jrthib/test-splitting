5883ea60ceb60ca25960d23be6758c2e8bb98d4b
Joseph Thibeault <joseph.thibeault@gmail.com>
2019-08-17 22:03:48 -0700

    edit service 1 (#2)
    * edit service 1
    
    * update circle config

--- .circleci/test-split.sh
+++ .circleci/test-split.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
 
 target_branch='master'
-CIRCLE_BRANCH='test-splitting'
+CIRCLE_BRANCH='edit-service-1'
 
 changed_files=$(git diff --name-only $target_branch..$CIRCLE_BRANCH)
-
-# echo $changed_files | tr ' ' '\n' > changed.txt
-
-# grep -v "^src\/services\/" changed.txt
+echo $changed_files
 
 # If common, models, or other files are affected, run full tests
-common_models_affected=$(echo $changed_files | tr ' ' '\n' | grep -oE "^src\/services\/(common|models)/")
-others_affected=$(echo $changed_files | tr ' ' '\n' | grep -v "^src\/services\/")
+common_models_files=$(echo $changed_files | tr ' ' '\n' | grep -oE "^src\/services\/(common|models)/" || true)
+common_models_affected=$(echo $common_models_files | awk "{print length}")
+others_files=$(echo $changed_files | tr ' ' '\n' | grep -v "^src\/services\/" || true)
+others_affected=$(echo $others_files | awk "{print length}")
+services=$(echo $changed_files | tr ' ' '\n' | grep -oE "^src\/services\/(.*)/" || true)
+services_affected=$(echo $services | awk "{print length}")
 
-if [ -z "$common_models_affected" ] || [ -z "$others_affected" ]; then
-    services_affected=$(echo $changed_files | tr ' ' '\n' | grep -oE "^src\/services\/(.*)/")
-    echo $services_affected
-    echo "run partial suite"
+if [ ${#services_affected} -gt 0 ] && [ ${#common_models_affected} -eq 0 ] && [ ${#others_affected} -eq 0 ]; then
+    npm test -- $services
 else
-    echo "run full suite"
+    npm test
 fi
-
